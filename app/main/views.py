656ca208701b3205a2026bbd78d6bec0e7282819
Preston-Too <toopreston@gmail.com>
2020-12-13 00:23:54 +0300

    added function to return index and profile

---
+++ app/main/views.py
@@ -0,0 +1,53 @@
+from flask import render_template,request,redirect,url_for,flash
+from . import main
+from .. import db,photos
+from flask_login import login_user,logout_user,login_required,current_user
+from ..requests import getQuotes
+from .forms import BlogForm,CommentForm,updateProfile
+from ..models import Blog,Comment,User
+
+@main.route('/',methods = ['GET'])
+def index():
+    getquotes = getQuotes()
+    return render_template ('index.html',getquotes = getquotes)
+
+
+    
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    blogs = Blog.query.filter_by(user_id = user.id).all()
+    if user is None:
+        abort(404)
+
+    return render_template('profile/profile.html',user = user,blogs=blogs)  
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = updateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+           
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)  
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))    
